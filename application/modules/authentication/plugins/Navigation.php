f10dddbe72a538c95179fc56d691a9b721a1cab2
Abimael Rodriguez <abimaelrc@gmail.com>
2013-01-23 17:45:09 -0400

    Add property $_checkFullPath to navigation plugin
    Delete a class that is not use in framework

--- application/modules/authentication/plugins/Navigation.php
+++ application/modules/authentication/plugins/Navigation.php
@@ -5,28 +5,45 @@ class Authentication_Plugin_Navigation extends Zend_Controller_Plugin_Abstract
 	private $_auth;
 	private $_view;
 	private $_navigation;
+	private $_checkFullPath;
 
-	public function __construct(Zend_Acl $acl,
-								Zend_Auth $auth,
-								Zend_View $view,
-								Zend_Navigation $navigation)
-	{
-		$this->_acl 	   = $acl;
-		$this->_auth 	   = $auth;
-		$this->_view 	   = $view;
-		$this->_navigation = $navigation;
+
+
+
+	public function __construct(Zend_Acl $acl, Zend_Auth $auth, Zend_View $view, Zend_Navigation $navigation, $checkFullPath = true){
+		$this->_acl 	      = $acl;
+		$this->_auth 	      = $auth;
+		$this->_view 	      = $view;
+		$this->_navigation    = $navigation;
+		$this->_checkFullPath = $checkFullPath;
 	}
+
+
+
 
 	public function preDispatch(Zend_Controller_Request_Abstract $request)
 	{
-		$resource = '/' . $request->getModuleName()
-					. '/' . $request->getControllerName()
-					. '/' . $request->getActionName();
-
-		$pages = new RecursiveIteratorIterator( $this->_navigation, RecursiveIteratorIterator::SELF_FIRST );
+		$uriModule     = null;
+		$uriController = null;
+		$uriAction     = null;
+		$resource      = '/' . $request->getModuleName()
+				       . '/' . $request->getControllerName()
+				       . '/' . $request->getActionName();
+		$pages         = new RecursiveIteratorIterator( $this->_navigation, RecursiveIteratorIterator::SELF_FIRST );
 
 		foreach($pages as $page){
-			if( $page->getHref() == $resource ){
+			$uriResource = $page->getResource();
+
+			if( empty($uriResource) === false ){
+				list($uriModule, $uriController, $uriAction) = explode(':', $uriResource);
+			}
+
+			if( ( $this->_checkFullPath === true && $page->getHref() == $resource )
+			    || ( $this->_checkFullPath === false
+				     && empty($uriModule) === false 
+				     && ( $uriModule . $uriController ) == ( $request->getModuleName() . $request->getControllerName() )
+			       )
+			){
 				$page->setActive(true);
 				$page->setClass('current');
 				break;
@@ -34,8 +51,8 @@ class Authentication_Plugin_Navigation extends Zend_Controller_Plugin_Abstract
 		}
 
 		$role = ( $this->_auth->hasIdentity() )
-				? $this->_auth->getIdentity()->role
-				: 'guest';
+		      ? $this->_auth->getIdentity()->role
+		      : 'guest';
 
 		$this->_view->navigation($this->_navigation)
 			 ->setAcl($this->_acl)
