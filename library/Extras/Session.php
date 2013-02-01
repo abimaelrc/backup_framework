1b8fc239b09e224cffb4cddf61153cc36e8986a8
Abimael Rodríguez <abimaelrc@gmail.com>
2013-02-01 13:53:21 -0400

    Coding standard

--- library/Extras/Session.php
+++ library/Extras/Session.php
@@ -1,16 +1,16 @@
 <?php
 class Extras_Session
 {
-	const NS = 'test';
+    const NS = 'test';
 
-	/**
+    /**
      * @param $options array
      * @param $sessionExpire int
-	 *     Default 1440 = 24 minutes
+     *     Default 1440 = 24 minutes
      * @return Zend_Session_Namespace
-	 */
-	public static function sessionNamespace(array $options = array(), $sessionExpire = 1440)
-	{
+     */
+    public static function sessionNamespace(array $options = array(), $sessionExpire = 1440)
+    {
         Zend_Session::start();
 
         $sessionOptions = array();
@@ -27,19 +27,19 @@ class Extras_Session
         }
         Zend_Session::setOptions($sessionOptions);
 
-		$session = new Zend_Session_Namespace(self::NS);
-		$session->setExpirationSeconds($sessionExpire);
+        $session = new Zend_Session_Namespace(self::NS);
+        $session->setExpirationSeconds($sessionExpire);
 
-		return $session;
-	}
+        return $session;
+    }
 
     /**
      * @return Zend_Auth_Storage_Session
      */
-	public static function storageNamespace()
-	{
-		$session = new Zend_Auth_Storage_Session(self::NS);
+    public static function storageNamespace()
+    {
+        $session = new Zend_Auth_Storage_Session(self::NS);
 
-		return $session;
-	}
+        return $session;
+    }
 }
